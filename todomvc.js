a84f9b151eedbcd57ddc595051d17a68976b8d6d
imdonguk <com8157@naver.com>
2018-07-19 17:02:55 +0900

    Add : 리스트폴더 기능 추가 Modify : 굳이 클래스를 쓰지않아도 되는 기능 함수로 구현

--- todomvc.js
+++ todomvc.js
@@ -10,17 +10,13 @@ class TodoModel{
     }
 }
 class TodoControll{
-    constructor(model,inputView, listView){
+    constructor(model,inputView){
         this.model = model;
         this.inputView = inputView;
-        this.listView = listView;
-        this.init();
-    }
-    init(){
-        this.inputView.addTodoHandler = this.addTodoHandler.bind(this);
+        this.inputView.addTodoHandler = this.addTodoHandler.bind(this);        
     }
     addTodoHandler(inputText){
-        this.listView.addList(inputText);
+        addList(inputText);
         this.model.setTodo(inputText);
     }
 }
@@ -29,16 +25,13 @@ class InputView{
     constructor(){
         this.addTodoHandler = null;
         this.inputElement = document.querySelector(".todo");
-        this.init();
-    }
-    init(){
         this.clickBtn();
     }
     clickBtn(){
         const enrollBtn = document.querySelector(".enroll");
         enrollBtn.addEventListener("click", ()=>{
             const inputText = this.inputElement.value;
-            debugger;
+            //debugger;
             this.addTodoHandler(inputText);
             this.resetInput();
         })
@@ -48,22 +41,37 @@ class InputView{
     }
 }
 
-class ListView{
-    constructor(){
+function addList(inputText){
+    const todoList = document.querySelector(".todolist");
+    const list = document.createElement("li");
+    const textNode = document.createTextNode(inputText);
+    list.appendChild(textNode);
+    todoList.appendChild(list);
+}
 
+class ListFolderView{
+    constructor(){
+        this.todoListElement = document.querySelector(".todolist");
+        this.parentElement = this.todoListElement.parentElement; 
+        this.folderBtn = document.querySelector(".folder");
     }
-    init(){
-        
+    clickBtn(){
+        this.folderBtn.addEventListener("click", e =>{
+            e.target.innerText === "접기" ? 
+            (this.hideList(), e.target.innerText = "펼치기")
+            : (this.showList(), e.target.innerText = "접기")
+        })
     }
-    addList(inputText){
-        const todoList = document.querySelector(".todolist");
-        const list = document.createElement("li");
-        const textNode = document.createTextNode(inputText);
-        list.appendChild(textNode);
-        todoList.appendChild(list);
+    hideList(){
+        this.todoListElement.remove();
+    }
+    showList(){
+        this.parentElement.appendChild(this.todoListElement);
     }
 }
+
 const todoModel = new TodoModel();
 const inputView = new InputView();
-const listView = new ListView();
-const controller = new TodoControll(todoModel,inputView,listView);
+const controller = new TodoControll(todoModel,inputView);
+const listFolderView = new ListFolderView();
+listFolderView.clickBtn();
